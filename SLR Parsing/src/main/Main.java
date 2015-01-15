415f7b0c657e9087f2c74525b11d7815c3108ffc
Vlad Cantor <rapp93_rod@yahoo.com>
2015-01-15 19:09:04 +0200

    two parts

--- SLR Parsing/src/main/Main.java
+++ SLR Parsing/src/main/Main.java
@@ -1,18 +1,35 @@
 package main;
 
+import javax.naming.PartialResultException;
+
 import LexicalScanner.LexicalScanner;
+import ScanningResult.ScanningResult;
 import slr.TableGenerator;
 import domain.Grammar;
 
 public class Main {
 	public static void main (String args[]) {
+		PartOne();
+		//PartTwo();
+	}
+	
+	private static void PartTwo(){
 		try {
 			Grammar g = new Grammar("grammar.txt");
 			String filename = "file.txt";
-			System.out.println("Scanning file: " + filename);
 			
-			LexicalScanner.Scan(filename);
+			ScanningResult lexicalScaner = LexicalScanner.Scan(filename);
 			//System.out.print(g.getStartingSymbol());
+			TableGenerator.generateTable(g);
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	private static void PartOne(){
+		try {
+			Grammar g = new Grammar("grammar.txt");
+			
 			TableGenerator.generateTable(g);
 		} catch (Exception e){
 			e.printStackTrace();
